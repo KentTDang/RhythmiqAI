1c2d197e9d17893babbacad6dc1702be6dfd5485
mtomilson <mtomilson51@gmail.com>
2024-04-02 00:06:11 -0400

    everything works pushing to develop

--- rhythmiq-ai/src/App.js
+++ rhythmiq-ai/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Dashboard from './Components/Dashboard'
+import Dashboard from './Components/Dashboard.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
