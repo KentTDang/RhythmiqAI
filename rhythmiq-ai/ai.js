3cd0ed45cbc8b48c485883c0d7ccac85a4665c18
mtomilson <mtomilson51@gmail.com>
2024-04-01 23:37:17 -0400

    chat gpt ai suggesting songs works

--- rhythmiq-ai/ai.js
+++ rhythmiq-ai/ai.js
@@ -1,15 +1,18 @@
 import { config } from "dotenv"
 config()
 
-import {Configuration, OpenAIApi} from "openai"
+import OpenAI from "openai"
 
-const openai = new OpenAIApi(new Configuration({
+const openai = new OpenAI({
     apiKey: process.env.API_KEY
-}))
-
-openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: "Hello ChatGPT"}]
-}).then(res => {
-    console.log(res)
 })
+
+
+
+openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: "Songs similar to 'Drivers license' by Olivia Rodrigo"}]
+}).then(res => {
+    console.log(res.choices[0].message.content)
+})
+
