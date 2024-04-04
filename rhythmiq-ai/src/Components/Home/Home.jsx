9d10a1e22fef16aeafb0a1e8a874659e321c04bc
Kent Dang <kenttdang@gmail.com>
2024-04-03 21:51:50 -0400

    Import fonts and create home page

--- rhythmiq-ai/src/Components/Home/Home.jsx
+++ rhythmiq-ai/src/Components/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import '../../index.css';
 import "./Home.css"
 
 export default function Home() {
   return (
+    <>
     <div className="nav-bar">
         
     </div>
+    <div className="header-section">
+        <h1>Spread Your Love For Music With Friends</h1>
+    </div>
+    </>
   )
 }
