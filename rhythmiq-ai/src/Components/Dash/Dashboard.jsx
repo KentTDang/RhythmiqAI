720379f4cae00d0132ef87bdb16be426f6954b60
Kent Dang <kenttdang@gmail.com>
2024-04-04 21:52:45 -0400

    Implement landing page

--- rhythmiq-ai/src/Components/Dash/Dashboard.jsx
+++ rhythmiq-ai/src/Components/Dash/Dashboard.jsx
@@ -3,6 +3,7 @@ import { firestore } from '../../Configs/firebase'
 import { addDoc, collection, onSnapshot, updateDoc, deleteDoc, doc } from 'firebase/firestore'
 import "./Dashboard.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import bg from '../../Assets/bg.webp'
 import {
   faArrowUp,
   faArrowDown,
@@ -93,6 +94,58 @@ export default function Dashboard() {
 
   return (
     <>
+
+      <div class="nav">
+        <div class="nav-links">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Work</a>
+          <a href="">Contact</a>
+        </div>
+      </div>
+
+      <div className="hero">
+      <h1>Share Your <br/>Love for <span>Music</span> <br/>With Friends.
+        </h1>
+        <div className="hero-stars">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1680" height="730" viewBox='0 0 1680 730' fill='none'>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.873077 0.487583 -0.78755 0.61625 429.154 175.316)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.873077 0.487583 -0.78755 0.61625 429.154 175.316)" fill="white"></circle>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 1504.14 366.363)" fill="white"></ellipse>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 322.141 513.363)" fill="white"></ellipse>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.994947 -0.100399 0.322552 0.946552 440 282.121)" fill="white"></circle>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 1553.78 554.305)" fill="white"></ellipse>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 250.141 326.566)" fill="white"></ellipse>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 280.908 307.363)" fill="white"></ellipse>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.994947 -0.100399 0.322552 0.946552 410.404 338.926)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(-0.322553 -0.946552 0.994947 -0.100399 1154.39 545.258)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(-0.322553 -0.946552 0.994947 -0.100399 995.66 552.828)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.994947 -0.100399 0.322552 0.946552 576 145.121)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(-0.322553 -0.946552 0.994947 -0.100399 814.387 514.258)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.061383 -0.998114 0.99741 0.07192 961.715 604.32)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.322553 0.946552 -0.994947 0.100399 861.078 260.352)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.322553 0.946552 -0.994947 0.100399 974.557 162.211)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.322553 0.946552 -0.994947 0.100399 544.891 280.938)" fill="white"></circle>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 1140.55 485.527)" fill="white"></ellipse>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.322553 0.946552 -0.994947 0.100399 989.111 172.809)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(0.322553 0.946552 -0.994947 0.100399 1245.01 314.668)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(-0.958827 0.28399 -0.613701 -0.789538 586.887 448.488)" fill="white"></circle>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 108.141 433.363)" fill="white"></ellipse>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(-0.958827 0.28399 -0.613701 -0.789538 1469.98 192.406)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(-0.958827 0.28399 -0.613701 -0.789538 1327.66 447.223)" fill="white"></circle>
+          <circle cx="0.6" cy="0.6" r="0.6" transform="matrix(-0.958827 0.28399 -0.613701 -0.789538 1308.95 468.746)" fill="white"></circle>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 320.141 360.363)" fill="white"></ellipse>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 1086.54 295.914)" fill="white"></ellipse>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 1285.14 263.363)" fill="white"></ellipse>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 1343.31 263.105)" fill="white"></ellipse>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 696.141 281.363)" fill="white"></ellipse>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 630.725 434.363)" fill="white"></ellipse>
+          <ellipse cx="0.6" cy="0.6" rx="0.6" ry="0.6" transform="matrix(-0.95101 -0.309159 0.561017 -0.827804 1372.09 492.215)" fill="white"></ellipse>
+        </svg>
+        <img src={bg} className='img'/>
+        </div>
+        </div>
+
       <div className='song-review'>
         <form onSubmit={handleSave}>
           <label>Song Review</label>
@@ -142,7 +195,7 @@ export default function Dashboard() {
             songReviews.map((songs) => (
               <div className="review" key={songs.id}>
                 <p>{songs.song} - {songs.album} - {songs.artist} - {songs.review}
-                {[...Array(5)].map((_, index) => {
+                  {[...Array(5)].map((_, index) => {
                     const currentRating = index + 1;
                     return (
                       <FontAwesomeIcon
