51197e002990735bc08f162b45eb44f0cf3e0867
Kent Dang <kenttdang@gmail.com>
2024-04-06 02:52:08 -0400

    Remove Unused Code

--- rhythmiq-ai/src/App.js
+++ rhythmiq-ai/src/App.js
@@ -5,9 +5,6 @@ import { Song } from "./Components/Song.js";
 import { Review } from "./Components/Review.js"
 import Dashboard from './Components/Dash/Dashboard.jsx'
 
-// import { reviews } from "./Components/reviews";
-// import { Contact } from "./Components/Contact";
-// import { Footer } from "./Components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
