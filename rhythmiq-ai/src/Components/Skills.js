352d1bf09ec990c70c843146b1b3431a5cd5b265
Kent Dang <kenttdang@gmail.com>
2024-04-06 01:56:14 -0400

    Implement carousel

--- rhythmiq-ai/src/Components/Skills.js
+++ rhythmiq-ai/src/Components/Skills.js
@@ -1,6 +1,11 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules'
+import headerImg from './../Assets/img/header-img.svg'
 
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
 import { Row, Container, Col } from 'react-bootstrap'
 
 export const Skills = () => {
@@ -27,19 +32,63 @@ export const Skills = () => {
 
   return (
     <section className='skill' id='skills'>
+      
       <Container>
         <Row>
           <Col>
             <div className="skill-bx">
               <h2>
-                Skills
+                Trending Songs
               </h2>
               <p>FAT FAT FAT FAT FATFA FATFTGSYTBXUBWUYBXU</p>
-              {/* <Carousel responsive={responsive} infinite={true} className='skill-slider'> <div>Item 1</div>
-                <div>Item 2</div>
-                <div>Item 3</div>
-                <div>Item 4</div>
-              </Carousel> */}
+              <Swiper 
+              effect={'coverflow'} 
+              grabCursor={true} 
+              centeredSlides={true} 
+              loop={true} 
+              slidesPerView={'auto'} 
+              coverflowEffect={
+                {
+                  rotate: 0,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 2.5
+                }
+              }
+              paginatio={{el:'.swiper-pagination',clickable:true}}
+              navigation={{
+                nextEl:'.swiper-button-next',
+                prevEl:'.swiper-button-prev',
+                clickable: true,
+              }}
+              modules={[EffectCoverflow,Pagination,Navigation]} 
+              className='swiper_container'>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={headerImg} alt="slide_image"/>
+                </SwiperSlide>
+
+                <div className="slider-container">
+                  <div className="swiper-button-prev slider-arrow">
+                    <ion-icon name="arrow-back-outline"></ion-icon>
+                  </div>
+                  <div className="swiper-button-next slider-arrow">
+                    <ion-icon name="arrow-forward-outline"></ion-icon>
+                  </div>
+                </div>
+              </Swiper>
+
             </div>
           </Col>
         </Row>
