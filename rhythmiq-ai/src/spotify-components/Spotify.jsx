28ef3a125605121f38646ba51870fcf870a16534
mtomilson <mtomilson51@gmail.com>
2024-05-21 00:11:35 -0400

    creates a function to login users and authenticate through spotify using the top right login button!

---
+++ rhythmiq-ai/src/spotify-components/Spotify.jsx
@@ -0,0 +1,13 @@
+
+import {useEffect, useState} from 'react'
+
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+
+export function handleLogin() {
+    window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
+}
+
