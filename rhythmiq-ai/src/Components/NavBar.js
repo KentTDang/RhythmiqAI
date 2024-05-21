812c0c23d397448b9e7bc9f45f7865a29295bcac
Kent Dang <kent_2015@outlook.com>
2024-05-20 23:45:28 -0400

    🗑️Delete stuff we dont need

--- rhythmiq-ai/src/Components/NavBar.js
+++ rhythmiq-ai/src/Components/NavBar.js
@@ -1,9 +1,5 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../Assets/img/logo.svg';
-import navIcon1 from './../Assets/img/nav-icon1.svg';
-import navIcon2 from './../Assets/img/nav-icon2.svg';
-import navIcon3 from './../Assets/img/nav-icon3.svg';
 
 import {
   BrowserRouter as Router
@@ -37,7 +33,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            {/* Search bar goes here */}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -49,11 +45,6 @@ export const NavBar = () => {
               <Nav.Link href="#reviews" className={activeLink === 'reviews' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('reviews')}>Reviews</Nav.Link>
             </Nav>
             <span className="navbar-text">
-              <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
-              </div>
                 <button className="vvd"><span>Let’s Connect</span></button>
             </span>
           </Navbar.Collapse>
