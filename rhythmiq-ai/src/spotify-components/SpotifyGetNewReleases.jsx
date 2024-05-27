284dac465e7c9f0b0baaf53f93c9663f65c90f78
Kent Dang <kent_2015@outlook.com>
2024-05-27 14:20:25 -0400

    🏗️Update data to be an array to match syntax

--- rhythmiq-ai/src/spotify-components/SpotifyGetNewReleases.jsx
+++ rhythmiq-ai/src/spotify-components/SpotifyGetNewReleases.jsx
@@ -4,37 +4,36 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export default function SpotifyGetNewReleases() {
-
-  const NEW_RELEASES_ENDPOINT = "https://api.spotify.com/v1/browse/new-releases";
-  const [token, setToken]= useState("");
-  const [data, setData] = useState({});
+  const NEW_RELEASES_ENDPOINT =
+    "https://api.spotify.com/v1/browse/new-releases";
+  const [token, setToken] = useState("");
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    if(localStorage.getItem("token")) {
-        setToken(localStorage.getItem("token"));
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
     }
-  },[])
+  }, []);
 
-  const handleGetNewReleases = async() => {
+  const handleGetNewReleases = async () => {
     try {
-        const response = await axios.get(NEW_RELEASES_ENDPOINT, {
-            headers : {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        console.log(response.data);
-        setData(response.data);
-    } catch(error) {
-        console.error("Failed fetching new releases: ", error);
+      const response = await axios.get(NEW_RELEASES_ENDPOINT, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setData(response.data.albums.items);
+    } catch (error) {
+      console.error("Failed fetching new releases: ", error);
     }
-    
-  }
+  };
 
   return (
-  <>
-  <button onClick={handleGetNewReleases}>Get New Releases</button>
-  {data?.albums?.items ? data.albums.items.map((item) => <p key={item.id}>{item.name}</p>) : null}
-  </>
-  
+    <>
+      <button onClick={handleGetNewReleases}>Get New Releases</button>
+      {data.map((song) => (
+        <p key={song.id}>{song.name}</p>
+      ))}
+    </>
   );
 }
